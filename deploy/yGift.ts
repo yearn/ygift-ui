f9a722b32873ee7a93cb1c15807d98fe8cdb10f2
Eric Juta <ericjohnjuta@gmail.com>
2020-11-06 10:56:07 +0000

    feat: get hardhat node and frontend running

--- deploy/yGift.ts
+++ deploy/yGift.ts
@@ -1,16 +1,11 @@
-module.exports = async ({
-  getNamedAccounts,
-  deployments,
-  getChainId,
-  getUnnamedAccounts,
-}) => {
+module.exports = async ({ getNamedAccounts, deployments, getChainId, getUnnamedAccounts }) => {
   const { deploy } = deployments;
   const { deployer } = await getNamedAccounts();
 
   // the following will only deploy "GenericMetaTxProcessor" if the contract was never deployed or if the code changed since last deployment
-  await deploy("Greeter", {
+  await deploy("yGift", {
     from: deployer,
     // gas: 4000000,
-    args: ["Greeting set from ./deploy/Greeter.ts"],
+    // args: ["yGift set from ./deploy/yGift.ts"],
   });
 };
