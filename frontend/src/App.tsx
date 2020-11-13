2e7ad07649e90d0cca14ab80fbcd74ae74c0741c
Eric Juta <ericjohnjuta@gmail.com>
2020-11-13 01:45:43 +0000

    feat: add ChakraProvider to app

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -8,26 +8,29 @@ import { CreateGift } from "./components/CreateGift";
 import { Layout } from "./components/Layout";
 import { About } from "./components/About";
 import { ViewGift } from "./components/ViewGift";
+import { ChakraProvider } from "@chakra-ui/core";
 
 function App() {
   return (
     <div className="App">
       <Router>
-        <HardhatContext>
-          <Layout>
-            <Switch>
-              <Route exact path="/">
-                <About></About>
-              </Route>
-              <Route path="/create-gift">
-                <CreateGift />
-              </Route>
-              <Route path="/gift/:tokenId">
-                <ViewGift />
-              </Route>
-            </Switch>
-          </Layout>
-        </HardhatContext>
+        <ChakraProvider>
+          <HardhatContext>
+            <Layout>
+              <Switch>
+                <Route exact path="/">
+                  <About></About>
+                </Route>
+                <Route path="/create-gift">
+                  <CreateGift />
+                </Route>
+                <Route path="/gift/:tokenId">
+                  <ViewGift />
+                </Route>
+              </Switch>
+            </Layout>
+          </HardhatContext>
+        </ChakraProvider>
       </Router>
     </div>
   );
