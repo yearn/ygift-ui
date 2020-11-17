d6fa5c82fc42fc4ec673899cb7bd36991c732b73
Eric Juta <ericjohnjuta@gmail.com>
2020-11-17 22:14:12 +0000

    refactor: add catch all unhandled exceptions, Push to /error page

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,15 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
+
+window.onerror = (event: Event | string, source?: string, lineno?: number, colno?: number, error?: Error) => {
+  console.error(event);
+  console.error(error);
+
+  setTimeout(() => {
+    window.location.href = "/error";
+  }, 3000);
+};
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
