35e819b0b9e8040dbba755b98959773662c1bff4
Eric Juta <ericjohnjuta@gmail.com>
2020-11-29 22:03:01 +0000

    revert: revert useCreateGiftOrder for gift id

--- frontend/src/components/CreateGift/useCreateGiftFormManagement/index.tsx
+++ frontend/src/components/CreateGift/useCreateGiftFormManagement/index.tsx
@@ -6,9 +6,9 @@ import { BigNumber, ethers } from "ethers";
 import { useHistory } from "react-router-dom";
 
 export function useCreateGiftFormManagement() {
+  const [yGift] = useContext(yGiftContext);
   const [currentAddress] = useContext(CurrentAddressContext);
   const [provider] = useContext(ProviderContext);
-  const [yGift] = useContext(yGiftContext);
   const [hasSubmitted, setHasSubmitted] = useState(false);
   const [giftCreatedId, setGiftCreatedId] = useState("");
   const _start = Math.floor(DateTime.local().toSeconds());
@@ -16,27 +16,26 @@ export function useCreateGiftFormManagement() {
   const history = useHistory();
 
   const submitHandler = async (params: Parameters<YGift["mint"]>) => {
+    console.log(params);
     return new Promise(async (resolve) => {
-      try {
-        console.log(params);
-        // Convert ether to gwei
-        console.log(params[2]);
-        params[2] = ethers.utils.parseEther(params[2].toString());
-        console.log(params[2]);
-        // Resolve ens for _to and _token
-        params[0] = (await provider?.resolveName(params[0])) || params[0];
-        params[1] = (await provider?.resolveName(params[1])) || params[1];
-        // Convert days to seconds
-        console.log(params[7]);
-        params[7] = dayInSeconds * Number(params[7]);
-        console.log(params[7]);
+      // Convert ether to gwei
+      console.log(params[2]);
+      params[2] = ethers.utils.parseEther(params[2].toString());
+      console.log(params[2]);
+      // Resolve ens for _to and _token
+      params[0] = (await provider?.resolveName(params[0])) || params[0];
+      params[1] = (await provider?.resolveName(params[1])) || params[1];
+      // Convert days to seconds
+      console.log(params[7]);
+      params[7] = dayInSeconds * Number(params[7]);
+      console.log(params[7]);
 
+      try {
         const gasLimit = await yGift?.instance?.estimateGas.mint.apply(null, params as any);
         const tx = yGift?.instance?.mint.apply(null, params.concat({ gasLimit: gasLimit?.add("80000") }) as any);
         const createGiftTx = await tx;
         await createGiftTx?.wait();
         setHasSubmitted(true);
-
         const giftMintedSentEventFilter = yGift?.instance?.filters?.GiftMinted(
           String(currentAddress),
           null,
@@ -50,11 +49,11 @@ export function useCreateGiftFormManagement() {
             console.log(giftsMinted);
             const giftMinted = giftsMinted?.[giftsMinted.length - 1];
             setGiftCreatedId(giftMinted?.[2]);
-            resolve(true);
           }
         }
         // const parsedLogs = result?.logs.map((log) => yGift?.instance?.interface?.parseLog(log)?.args);
         // console.log(parsedLogs);
+        resolve(true);
       } catch (e) {
         console.error(e);
         history.push("/error");
